5507be4d239ed823367cde6cd575c2f1fb5991f2
김동욱 <smartpodo@kakao.com>
2023-03-13 12:51:28 +0900

    [BE] add : add ResponseDto

--- src/main/java/com/ssafy/ttocket/dto/ResponseDto.java
+++ src/main/java/com/ssafy/ttocket/dto/ResponseDto.java
@@ -1,2 +1,24 @@
-package com.ssafy.ttocket.dto;public class ResponseDto {
+package com.ssafy.ttocket.dto;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ResponseDto<T> {
+
+    @JsonProperty("status_code")
+    private int statusCode;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("body")
+    private T body;
 }
