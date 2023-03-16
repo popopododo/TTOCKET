f81c7871c7a05df13ac7bad34dfa252abf62f08b
김경민 <starlight3775@gmail.com>
2023-03-16 15:49:59 +0900

    [BE] add : clickLike

---
+++ back/src/main/java/com/ssafy/ttocket/repository/querydsl/UserRepositoryImpl.java
@@ -0,0 +1,6 @@
+package com.ssafy.ttocket.repository;
+
+import org.springframework.data.jpa.repository.support.QuerydslRepositorySupport;
+
+public class UserRepositoryImpl extends QuerydslRepositorySupport {
+}
