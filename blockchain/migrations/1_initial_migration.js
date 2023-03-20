f69363c36df031febfccc5ff9301cf577168a10a
smartpodo@kakao.com <smartpodo@kakao.com>
2023-03-20 09:20:39 +0900

    UPDATE Folder Directories

--- blockchain/migrations/1_initial_migration.js
+++
@@ -1,8 +0,0 @@
-const SangToken = artifacts.require("SangToken");
-const Ticket = artifacts.require("Ticket");
-
-module.exports = async function (deployer) {
-  await deployer.deploy(SangToken, "SANGMINTOKEN", "SANGMIN");
-  await deployer.deploy(Ticket);
-
-};
--- frontend/README.md
+++
@@ -1,6 +0,0 @@
-# 실행 방법
-
-```bash
-npm install
-npm start
-```
