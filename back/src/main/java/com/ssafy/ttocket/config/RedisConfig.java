6f5c311607f4a9c14400695f356d75a5907eb46c
정상민 <jsangmin4175@gmail.com>
2023-03-21 16:33:57 +0900

    [BE] add: 좌석상태 변경 with cache

--- back/src/main/java/com/ssafy/ttocket/config/RedisConfig.java
+++ back/src/main/java/com/ssafy/ttocket/config/RedisConfig.java
@@ -17,9 +17,15 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory();
+        lettuceConnectionFactory.setHostName(host);
+        lettuceConnectionFactory.setPort(port);
+        lettuceConnectionFactory.setPassword(password);
+        return lettuceConnectionFactory;
     }
 
     @Bean
--- back/src/main/java/com/ssafy/ttocket/controller/PerformanceController.java
+++ back/src/main/java/com/ssafy/ttocket/controller/PerformanceController.java
@@ -68,7 +68,7 @@ public class PerformanceController {
         return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
     }
 
-    @GetMapping("/reserve/{performanceId}")  // 예매하기 버튼 클릭
+    @GetMapping("/reserve/{performanceId}")  // 공연 상세보기에서 예매하기 버튼 클릭
     public ResponseEntity<ResponseDto> performanceReservation(@PathVariable int performanceId) {
         log.info("request performance reservation return");
         ResponseDto responseDto = performanceService.reservationState(performanceId);
@@ -79,7 +79,7 @@ public class PerformanceController {
     public ResponseEntity<ResponseDto> performanceReservation(@PathVariable int performanceId,
                                                               @PathVariable int seatId,
                                                               @PathVariable int code) {
-        log.info("request performance reservation return");
+        log.info("click seatNo or cancel seat return");
         ResponseDto responseDto = performanceService.changeReservationState(performanceId, seatId, code);
         return new ResponseEntity<ResponseDto>(responseDto, HttpStatus.OK);
     }
