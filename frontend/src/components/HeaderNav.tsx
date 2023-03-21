8dd9ae648ba71d2ec01e4a41634efbcda1ffd63e
HyeonIn <gusdlsdlek@gmail.com>
2023-03-21 11:20:13 +0900

    S08P22B210-186 [FE] 상단, 하단 네비게이션 구현

--- frontend/src/components/HeaderNav.tsx
+++ frontend/src/components/HeaderNav.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 
 export default function HeaderNav() {
   return (
-    <div className='flex content-between mb-4'>
-        <p className='flex-1 text-2xl'>똑켓</p>
-        <p className='flex mt-3'>알림</p>
+    <div className='absolute top-0 z-10 flex content-between w-screen px-2 pt-4 bg-white'>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 mt-3">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+        </svg>
+        <p className='flex-1 text-4xl text-center HeaderNavTitle'>똑켓</p>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-8 h-8 mt-3">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+        </svg>
+
     </div>
   )
 }
