92affad6b11ea118917aa6a35b1084b7ab7e8eab
정상민 <jsangmin4175@gmail.com>
2023-03-24 11:06:29 +0900

    [BE] modify: WebConfig

--- back/src/main/java/com/ssafy/ttocket/config/WebMvcConfig.java
+++ back/src/main/java/com/ssafy/ttocket/config/WebMvcConfig.java
@@ -9,7 +9,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").
-                allowedOrigins("j8b210.p.ssafy.io","https://localhost:3000")
+                allowedOrigins("*")
                 .allowedMethods("*");
     }
 }
