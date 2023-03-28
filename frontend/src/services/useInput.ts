e2811621ba97bec18af82de2f0eef69d37801dc6
sejin <tpwls86@naver.com>
2023-03-28 14:02:49 +0900

    [FE]fix: 공연 가격 전송내역 변경

--- frontend/src/services/useInput.ts
+++ frontend/src/services/useInput.ts
@@ -1,29 +1,3 @@
-// import { useState } from "react";
-
-// const useInput = (
-//   initialState: string,
-//   validator?: (value: string) => boolean
-// ) => {
-//   const [value, setValue] = useState(initialState);
-//   const onChange = (event: any) => {
-//     const {
-//       target: { value },
-//     } = event;
-//     if (validator === undefined) {
-//       setValue(value);
-//     } else {
-//       const willdata = validator(value);
-//       if (willdata) {
-//         setValue(value);
-//       }
-//     }
-//     setValue(value);
-//   };
-
-//   return { value, onChange };
-// };
-
-// export default useInput;
 import { useState } from "react";
 
 type UseInputReturnType = {
