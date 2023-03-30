5c435fd1678008d8f268a815ec352d045a6c9d01
김경민 <starlight3775@gmail.com>
2023-03-30 14:34:31 +0900

    [BE] modify: 스케줄러 수정

--- back/src/main/java/com/ssafy/ttocket/service/SchedulerService.java
+++ back/src/main/java/com/ssafy/ttocket/service/SchedulerService.java
@@ -39,32 +39,34 @@ public class SchedulerService {
             for (String canceledSeat : canceledSeats) {
                 int performanceId = Integer.parseInt(canceledSeat.substring(0, 1));
                 int seat_no = Integer.parseInt(canceledSeat.substring(canceledSeat.indexOf(":") + ":".length()));
-                Seat byPerformanceIdAndSeatId = seatRepository.findByPerformanceIdAndSeatId(performanceId, new SeatId(performanceId, seat_no));
-                byPerformanceIdAndSeatId.setStatus(SeatStatus.EMPTY);
+
+                String key = "seatStatus::" + performanceId;
+                listOperations.set(key, seat_no-1, SeatStatus.PURCHASED_CANCEL.toString());
             }
         }
         redisTemplate.delete(canceledList);
 
 
-//        // 람다식 적용 방법 찾아서 적용하기
-//        Set<String> keys = redisTemplate.keys("seatStatus::*");
-//        for (String key : keys) {
-//            String keyIdx = key.substring(12);  // performanceId 추출
-//            if (!redisTemplate.type(key).equals(DataType.NONE)) {
-//                List<Seat> targetSeats = seatRepository.findByPerformanceId(Integer.parseInt(keyIdx));  // 좌석 정보를 가져오기 위해 repository 탐색
-//                List<String> list = listOperations.range("seatStatus::" + keyIdx, 0, -1);  // 레디스
-//
-//                // 아래 조건에 걸리지 않는 경우 탐색 필요 -> 개발과정에서 mysql 개별 조작 가능성
-//                if (list.size() == targetSeats.size()) {
-//                    for (int i = 0; i < list.size(); i++) {
-//                        SeatStatus s = SeatStatus.valueOf(list.get(i));
-//                        targetSeats.get(i).setStatus(s);
-//                        seatRepository.save(targetSeats.get(i));
-//                    }
-//                }
-//            } else {
-//                log.debug("리스트 타입이 아님");
-//            }
-//        }
+        // 클래스 나눠서 시간대를 길게 매서드
+        // 람다식 적용 방법 찾아서 적용하기
+        Set<String> keys = redisTemplate.keys("seatStatus::*");
+        for (String key : keys) {
+            String keyIdx = key.substring(12);  // performanceId 추출
+            if (!redisTemplate.type(key).equals(DataType.NONE)) {
+                List<Seat> targetSeats = seatRepository.findByPerformanceId(Integer.parseInt(keyIdx));  // 좌석 정보를 가져오기 위해 repository 탐색
+                List<String> list = listOperations.range("seatStatus::" + keyIdx, 0, -1);  // 레디스
+
+                // 아래 조건에 걸리지 않는 경우 탐색 필요 -> 개발과정에서 mysql 개별 조작 가능성
+                if (list.size() == targetSeats.size()) {
+                    for (int i = 0; i < list.size(); i++) {
+                        SeatStatus s = SeatStatus.valueOf(list.get(i));
+                        targetSeats.get(i).setStatus(s);
+                        seatRepository.save(targetSeats.get(i));
+                    }
+                }
+            } else {
+                log.debug("리스트 타입이 아님");
+            }
+        }
     }
 }
