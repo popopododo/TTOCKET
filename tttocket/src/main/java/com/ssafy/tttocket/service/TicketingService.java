bfb64d718d200433faaa5761d46b0d1b60ac8794
정상민 <jsangmin4175@gmail.com>
2023-04-04 14:36:12 +0900

    [BE] modify: log

--- tttocket/src/main/java/com/ssafy/tttocket/service/TicketingService.java
+++ tttocket/src/main/java/com/ssafy/tttocket/service/TicketingService.java
@@ -17,7 +17,7 @@ public class TicketingService {
     public Map<String,Object> addToWaitQue(String userId, int performId){
         Map<String,Object> result = new HashMap<>();
         String key = "WaitQue::"+performId;
-        log.info("key :", key);
+        log.info("add key :"+ key);
 
         ListOperations<String, Object> listOperations = redisTemplate.opsForList();
         listOperations.rightPush(key,userId); // 넣을때는 rightPush 뺄때는 leftPop
